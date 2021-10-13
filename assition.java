81272014bd659a50e942b27e92c10e3bee95a39e
Avinash sahu <avinash@leewayhertz.com>
2021-10-13 07:32:12 +0530

    initial comit

--- assition.java
+++ assition.java
@@ -2,7 +2,7 @@ public class sum{
     public static void main(string[]args) {
        int a=10;
        int b= 10;
-       system.out.println("the sumis "+(a+b)); 
+       system.out.println("the sum is "+(a+b)); 
         
     }
 }
